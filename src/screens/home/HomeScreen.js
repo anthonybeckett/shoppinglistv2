6fdd9b34b6b6d203ea2f6d73906801e763cc0735
Anthony Beckett <antneyb10@gmail.com>
2022-10-20 22:14:15 +0100

    Updated gitignore, moved app component to a screen, updated tailwind config to work

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -2,14 +2,16 @@ import React from "react";
 import { SafeAreaView, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
-const App = () => {
+const HomeScreen = () => {
 	return (
 		<NavigationContainer>
 			<SafeAreaView>
-				<Text className="text-4xl font-bold text-blue-500">Hello</Text>
+				<Text className="text-4xl font-bold text-blue-400">
+					Working
+				</Text>
 			</SafeAreaView>
 		</NavigationContainer>
 	);
 };
 
-export default App;
+export default HomeScreen;
